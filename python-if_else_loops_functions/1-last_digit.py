757ae44ac6eee68d22a0e4136ff35fbe81739829
C-Mauro <maurocamila744@gmail.com>
2023-01-19 05:24:00 -0800

    fix typeErro

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = int(str(number)[-1])
+last_digit = number % 10 
 str1 = "Last digit of"
 str2 = "is"
 
@@ -11,8 +11,8 @@ if number > 0 and last_digit > 5:
 if last_digit == 0:
     print(f"{str1} {number} {str2} {last_digit} and is 0")
 
-if number < 0:
-    last_digit = -last_digit
+if number < 0 and last_digit != 0:
+    last_digit = -10 + last_digit
     print(f"{str1} {number} {str2} {last_digit} and is less than 6 and not 0")
 
 elif number > 0 and last_digit < 6 and last_digit != 0:
