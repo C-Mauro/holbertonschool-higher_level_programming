80fff8353f95c4ad49892494544f7590580e166d
C-Mauro <maurocamila744@gmail.com>
2023-01-19 07:42:09 -0800

    fix pycodestyle and negative last digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,6 @@
 def print_last_digit(number):
     last_digit = number % 10
     if number < 0:
-        last_digit = -10 + last_digit
+        last_digit = 10 - last_digit
     print(last_digit, end="")
     return last_digit
-
