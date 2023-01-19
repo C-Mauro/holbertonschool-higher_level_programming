3e09613568a3ec53b1729dce969faf8c7f6b56c5
C-Mauro <maurocamila744@gmail.com>
2023-01-19 05:05:38 -0800

    fix the end with ,

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 
 for j in range(0, 9):
     for i in range(0, 10):
-        if j < i and j != i:
+        if j < i and j != i and j != 8:
             print(f"{j}{i}".format(range), end=", ")
-
+    if j == 8:
+        print(f"{j}{i}".format(range))
