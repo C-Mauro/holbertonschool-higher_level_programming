2f8b753e79fca1ad95711283f93a0248786131a5
C-Mauro <maurocamila744@gmail.com>
2023-01-20 05:51:13 -0800

    first commit

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 100):
+        if i % 3 == 0:
+            print(f"Fizz", end=" ")
+        elif i % 5 == 0:
+            print(f"Buzz")
+        elif i % 5 == 0 and i % 3 == 0:
+            print(f"FizzBuzz", end=" ")
+        else: 
+            print(i, end=" ")
