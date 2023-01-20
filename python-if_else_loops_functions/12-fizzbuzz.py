b0b17df9839baeee610a940b9cf59a1c71d556eb
C-Mauro <maurocamila744@gmail.com>
2023-01-20 05:57:07 -0800

    fix space after buzz with end=

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 100):
-        if i % 3 == 0:
-            print(f"Fizz", end=" ")
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print(f"FizzBuzz", end=" ")
         elif i % 5 == 0:
             print(f"Buzz", end=" ")
-        elif i % 5 == 0 and i % 3 == 0:
-            print(f"FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print(f"Fizz", end=" ")
         else: 
             print(i, end=" ")
