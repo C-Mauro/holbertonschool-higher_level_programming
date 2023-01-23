5da0a5e0f4b30dc59ae89fa41ec694798326dab1
C-Mauro <maurocamila744@gmail.com>
2023-01-23 08:48:38 -0800

    first commit

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return None
+    if idx >= len(my_list):
+        return None
+    for i in range(len(my_list)):
+        if i == idx:
+            my_list[i] = element
+            return my_list
