4199c22d49ba53a3ed67f9328559c002b21d980b
C-Mauro <maurocamila744@gmail.com>
2023-01-23 08:52:46 -0800

    fix the returns

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,9 +2,9 @@
 
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return None
+        return my_list
     if idx >= len(my_list):
-        return None
+        return my_list
     for i in range(len(my_list)):
         if i == idx:
             my_list[i] = element
