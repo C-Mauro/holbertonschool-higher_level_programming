37caa757ecdd504c53e9e6ff8786a57463797c1a
C-Mauro <maurocamila744@gmail.com>
2023-01-24 13:39:56 -0800

    fix list = [] and idx == lenght of the list

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,9 @@
 
 
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list):
+    if my_list == []:
+        return []
+    if idx < 0 or idx > len(my_list) or idx == len(my_list):
         return my_list
     for i in range(len(my_list)):
         if i == idx:
