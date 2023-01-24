8731e0c104cacb3af76bbbad7b07ee114a6f6f76
C-Mauro <maurocamila744@gmail.com>
2023-01-24 09:26:36 -0800

    fix more than 2 elements in tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,8 +6,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     if len(tuple_a) < 2:
         tuple_a = tuple_a + zero
+    else:
+        tuple_a = tuple_a[:2]
     if len(tuple_b) < 2:
         tuple_b = tuple_b + zero
-
-    new_tuple = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+    else:
+        tuple_b = tuple_b[:2]
+    for i in range(len(tuple_a)):
+        new_tuple = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
     return new_tuple
