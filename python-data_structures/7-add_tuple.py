1f808ff135f847344a0fc48904bb4f40039f83c4
C-Mauro <maurocamila744@gmail.com>
2023-01-24 09:35:12 -0800

    fix the (1, ) + (1, )

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,15 +3,13 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     zero = (0, 0)
-
     if len(tuple_a) < 2:
         tuple_a = tuple_a + zero
-    else:
         tuple_a = tuple_a[:2]
     if len(tuple_b) < 2:
         tuple_b = tuple_b + zero
-    else:
         tuple_b = tuple_b[:2]
+    
     for i in range(len(tuple_a)):
         new_tuple = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
     return new_tuple
