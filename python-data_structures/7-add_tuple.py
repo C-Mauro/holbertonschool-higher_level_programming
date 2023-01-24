58ed0cfdb9d05aec41268a779d683875fe3f0a0b
C-Mauro <maurocamila744@gmail.com>
2023-01-24 09:06:15 -0800

    first commit

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    zero = (0, 0)
+
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + zero
+    if len(tuple_b) < 2:
+        tuple_b = tuple_b + zero
+
+    new_tuple = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+    return new_tuple
