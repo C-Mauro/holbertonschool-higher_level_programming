adc79312f4938b900e2880391b994979467c8d85
C-Mauro <maurocamila744@gmail.com>
2023-01-24 10:44:03 -0800

    fix the sentence =  case

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 
 def multiple_returns(sentence):
     len_sen = len(sentence)
-    
+    if len_sen < 1:
+        return len_sen, None
     for i in range(len_sen):
-        return len_sen, sentence[0]
-    
+        return len_sen, sentence[0] 
