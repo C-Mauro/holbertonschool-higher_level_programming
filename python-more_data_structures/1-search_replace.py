ae5531abebb6b143ebfad3f7afc0b60120cc5d71
C-Mauro <maurocamila744@gmail.com>
2023-01-25 07:45:03 -0800

    fix the return value and list=[] case

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,8 +2,11 @@
 
 
 def search_replace(my_list, search, replace):
+    if my_list == []:
+        return []
+
     new = my_list.copy()
     for i in range(len(my_list)):
         if new[i] == search:
             new[i] = replace
-            return new
+    return new
