252ff20e7fc6c1f0184dfb50178ce98c0e1b0291
C-Mauro <maurocamila744@gmail.com>
2023-01-28 03:38:23 -0800

    extra characters

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,5 @@ def print_matrix_integer(matrix=[[]]):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end="")
             if j != (len(matrix[i]) - 1):
-                print(" ", end="")
-        print(" ")
+                print("", end="")
+        print("")
