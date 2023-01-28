856cf24f7ecbed02cf5766e0ff61c3fd5f39a884
C-Mauro <maurocamila744@gmail.com>
2023-01-28 02:08:15 -0800

    first commit

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+def safe_print_list_integers(my_list=[], x=0):
+    size = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            size += 1
+        except (ValueError, TypeError) as Error:
+            continue
+    print()
+    return size
