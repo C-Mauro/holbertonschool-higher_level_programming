3b3c6656647894775e26d0f9f4e2a72edd54b47b
C-Mauro <maurocamila744@gmail.com>
2023-01-28 01:42:50 -0800

    fix except

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@
 
 def safe_print_list(my_list=[], x=0):
     size = 0
-    for i in range(0,x):
-        try: 
+    for i in range(0, x):
+        try:
             print("{}".format(my_list[i]), end="")
             size += 1
-        except: 
+        except IndexError:
             continue
-    print() 
+    print()
     return size
