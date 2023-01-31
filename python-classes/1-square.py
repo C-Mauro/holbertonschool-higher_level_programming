caaa2af41d8ddd5d9a077b9e4ea214d31895276c
C-Mauro <maurocamila744@gmail.com>
2023-01-30 17:19:40 -0800

    fix documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 '''A square class define size'''
 
+
 class Square:
-'''Define Class square with size attribute'''    
+    '''Define Class square with size attribute'''
     def __init__(self, size):
         '''initialize class square'''
         self.__size = size
