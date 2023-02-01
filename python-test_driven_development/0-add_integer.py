69357c57a1bc6ea433594d7c509e162ae77c538a
C-Mauro <maurocamila744@gmail.com>
2023-02-01 11:01:15 -0800

    fix some cases

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 '''Integers addition'''
 
-
 def add_integer(a, b=98):
     '''add int a and b'''
-    if type(a) == int and type(b) == int:
-        return a + b
-    elif type(a) == float:
-        return int(a) + b
-    elif type(b) == float:
-        return a + int(b)
+    if type(a) == float or type(b) == float:
+        return int(a) + int(b)
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    if type(b) != int:
+        raise TypeError("b must be an integer")
     else:
-        if type(a) is not int:
-            raise TypeError("a must be an integer")
-        if type(b) is not int:
-            raise TypeError("b must be an integer")
+        return a + b
