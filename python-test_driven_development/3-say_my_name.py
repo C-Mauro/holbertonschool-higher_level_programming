191382b1043028759f045a0babf6c44578da5979
C-Mauro <maurocamila744@gmail.com>
2023-02-02 07:08:26 -0800

    first commit

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" print my full name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """function to print your full name"""
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
