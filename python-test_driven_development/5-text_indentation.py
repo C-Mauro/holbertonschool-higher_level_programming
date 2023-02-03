3d4e82da7a627671445c910d29f6e78474d72ccb
C-Mauro <maurocamila744@gmail.com>
2023-02-03 09:23:26 -0800

    add tests

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """text indentation"""
 
-#(.?:) chars
+
 def text_indentation(text):
-    """Write a function that prints a text with 2 new lines after each of these characters: ., ? and :"""
+    """function that prints a text with 2 new lines after each ., ? and :"""
     if type(text) is not str:
         raise TypeError("text must be a string")
     new_t = ""
     for i in range(len(text)):
-        new_t += text[i] 
+        new_t += text[i]
         if text[i] == "." or text[i] == "?" or text[i] == ":":
-            new_t += '\n\n'
+            new_t += "\n\n"
     print(new_t, end="")
-
