dd595480b3288df30b8b0d755934d651b66f996d
C-Mauro <maurocamila744@gmail.com>
2023-02-03 08:34:20 -0800

    nothing

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,11 +6,10 @@ def text_indentation(text):
     """Write a function that prints a text with 2 new lines after each of these characters: ., ? and :"""
     if type(text) is not str:
         raise TypeError("text must be a string")
+    new_t = ""
     for i in range(len(text)):
+        new_t += text[i] 
         if text[i] == "." or text[i] == "?" or text[i] == ":":
-            print(text[i])
-            print()
-        else:
-            text.lstrip()
-            print(text[i], end="")
+            new_t += '\n\n'
+    print(new_t, end="")
 
