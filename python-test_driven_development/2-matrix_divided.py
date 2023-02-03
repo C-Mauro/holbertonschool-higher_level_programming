8c41befc58178f60815ce50466b30c2f55a0deba
C-Mauro <maurocamila744@gmail.com>
2023-02-03 07:08:36 -0800

    fix int_values function

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,9 +4,11 @@
 
 def matrix_divided(matrix, div):
 
-    """
-    Take the values of a matrix en divide
-    """
+    """Take the values of a matrix en divide"""
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
     err = "matrix must be a matrix (list of lists) of integers/floats"
     if check_matrix(matrix) is not True or int_values(matrix) is not True:
         raise TypeError(err)
@@ -15,26 +17,19 @@ def matrix_divided(matrix, div):
             pass
         else:
             raise TypeError("Each row of the matrix must have the same size")
-    if type(div) != int and type(div) != float:
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
     new_matrix = [[round((x / div), 2) for x in i] for i in matrix]
     return new_matrix
 
 
 def check_matrix(matrix):
     """check if a matrix is a list of lists"""
-    checked = all(isinstance(i, list) for i in matrix)
-    return checked
+    if all(isinstance(i, list) for i in matrix):
+        return True
 
 
 def int_values(matrix):
-    for i in matrix:
-        for j in i:
-            if type(j) is not int and type(j) is not float:
-                return False
-            else:
+    """ check if the values in matrix are float or int"""
+    for row in matrix:
+        for i in row:
+            if isinstance(i, (int, float)):
                 return True
-
-
