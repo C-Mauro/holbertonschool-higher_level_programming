fbf3f500c3dc67a34db4a403b3717784b1d9f645
C-Mauro <maurocamila744@gmail.com>
2023-02-05 10:03:42 -0800

    first commir

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''Definition of a rectangle'''
+
+
+class Rectangle:
+    '''define a rectangle atributes: width and height'''
+    def __init__(self, width=0, height=0):
+        '''initialize variables'''
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        ''' retrieve the data'''
+        self.__width = width
+
+    @width.setter
+    def width(self, value):
+        '''update data'''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        '''retrieve data'''
+        self.__height = height
+
+    @height.setter
+    def height(self, value):
+        '''update data'''
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
