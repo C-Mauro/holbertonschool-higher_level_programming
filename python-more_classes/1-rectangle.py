89667f6a4fb6fd39f1c6a290d5c9d6058e56d4ca
C-Mauro <maurocamila744@gmail.com>
2023-02-05 12:45:40 -0800

    type

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -12,12 +12,12 @@ class Rectangle:
     @property
     def width(self):
         ''' retrieve the data'''
-        self.__width = width
+        return self.__width
 
     @width.setter
     def width(self, value):
         '''update data'''
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -26,12 +26,12 @@ class Rectangle:
     @property
     def height(self):
         '''retrieve data'''
-        self.__height = height
+        return self.__height
 
     @height.setter
     def height(self, value):
         '''update data'''
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
