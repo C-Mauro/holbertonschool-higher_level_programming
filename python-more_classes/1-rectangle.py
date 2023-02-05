12cda55bd0424e182169d872394a69f103744d62
C-Mauro <maurocamila744@gmail.com>
2023-02-05 13:01:46 -0800

    fix __init__ remove underscore from the variables

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     '''define a rectangle atributes: width and height'''
     def __init__(self, width=0, height=0):
         '''initialize variables'''
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -17,7 +17,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         '''update data'''
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -31,7 +31,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         '''update data'''
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
