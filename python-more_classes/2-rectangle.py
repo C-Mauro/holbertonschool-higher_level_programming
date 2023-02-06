c5460df19b4ba7d1869152bcbf7732ca10a46acc
C-Mauro <maurocamila744@gmail.com>
2023-02-05 17:43:27 -0800

    fix rectangle(0, 0)

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -42,4 +42,6 @@ class Rectangle:
         return (self.__width * self.__height)
 
     def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return ((self.__width * 2) + (self.__height * 2))
