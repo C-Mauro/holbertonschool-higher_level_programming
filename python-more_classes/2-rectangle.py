53259cec612672eb351fb6c796a11f037a91294e
C-Mauro <maurocamila744@gmail.com>
2023-02-05 17:37:15 -0800

    first commit

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''Area and Perimeter'''
+
+
+class Rectangle:
+    '''defines a rectangle with atributte width and height'''
+
+    def __init__(self, width=0, height=0):
+        '''initialize variables'''
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        '''retrieve rectangle width'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''update the width'''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        '''retrieve rectangle height'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''update rectangle height '''
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise TypeError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        return ((self.__width * 2) + (self.__height * 2))
