82a4e46d5dcb4e8be1a4b56903e890e546f9f3a0
C-Mauro <maurocamila744@gmail.com>
2023-02-08 15:36:55 -0800

    first commit

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''same class or inherit from'''
+
+
+def is_kind_of_class(obj, a_class):
+    ''' return true if have the same class or inherit from"
+    if isinstance(obj, a_class):
+        return True
