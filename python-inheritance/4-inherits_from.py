8e7d40ec096fe2c9745045de82381e37b0a99ee2
C-Mauro <maurocamila744@gmail.com>
2023-02-08 15:50:09 -0800

    first commit

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Only sub class of'''
+
+
+def inherits_from(obj, a_class):
+    '''return True if is an instance of a class that inherited'''
+    return issubclass(a_class, a_class)
