a380a515cffe3346a012a7a99caec0105c8ffc37
C-Mauro <maurocamila744@gmail.com>
2023-02-09 07:38:44 -0800

    fix <name>, and add tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,6 +4,7 @@
 
 class BaseGeometry:
     '''integer validator'''
+
     def area(self):
         '''not define area'''
         raise Exception("area() is not implemented")
@@ -11,6 +12,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         '''check if the value is int'''
         if type(value) is not int:
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise TypeError("<name> must be greater than 0")
+            raise TypeError("{} must be greater than 0".format(name))
