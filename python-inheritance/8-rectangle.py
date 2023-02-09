7b8834b45391cd90d2970adccef1d84f8249635d
C-Mauro <maurocamila744@gmail.com>
2023-02-09 08:42:08 -0800

    fix super()

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -20,4 +20,5 @@ class BaseGeometry:
 class Rectangle(BaseGeometry):
     '''rectangle'''
     def __init__(self, width, height):
-        super().integer_validator(width, height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
