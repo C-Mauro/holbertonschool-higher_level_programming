00f058b9efeba93a44687ddee7c6672698f2e594
C-Mauro <maurocamila744@gmail.com>
2023-02-10 13:54:37 -0800

    fix not allowed to import module

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 '''Class to JSON'''
-import json
+from_json_string = __import__('4-from_json_string').from_json_string
+to_json_string = __import__('3-to_json_string').to_json_string
 
 
 def class_to_json(obj):
     '''returns the dictionary description'''
-    class_dic = json.dumps(obj.__dict__)
-    return json.loads(class_dic)
+    new_c = to_json_string(obj.__dict__)
+    return from_json_string(new_c)
