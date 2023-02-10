fafcd942c63dd1987d0bb32bdbcb9d2add8334a6
C-Mauro <maurocamila744@gmail.com>
2023-02-10 06:44:10 -0800

    first commit

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Square #1'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''class square'''
+    def __init__(self, size):
+        '''define square size'''
+        self.__size = size
+        super().integer_validator("size", size)
+
+    def area(self):
+        '''square area'''
+        area = self.__size * self.__size
+        return area
+
+    def __str__(self):
+        '''print operation'''
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
