c497e998e959e8b652060ef9638010087421eec5
C-Mauro <maurocamila744@gmail.com>
2023-02-10 06:29:49 -0800

    first commit

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' Rectangle'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''rectangle'''
+    def __init__(self, width, height):
+        '''initialize variables and check with integer_validator'''
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+
+    def area(self):
+        '''rectangle area'''
+        area = self.__width * self.__height
+        return area
+
+    def __str__(self):
+        '''print operation'''
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
