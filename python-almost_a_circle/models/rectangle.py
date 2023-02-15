51c034a58c48ca83465c2c9a9e4c23fc3b0801a4
C-Mauro <maurocamila744@gmail.com>
2023-02-15 15:52:28 -0800

    first commit

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+'''first rectangle'''
+from models.base import Base
+
+class Rectangle(Base):
+    '''class rectangle'''
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id=None)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+       
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")    
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        self.__x = value
+    
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        self.__y = value
+
