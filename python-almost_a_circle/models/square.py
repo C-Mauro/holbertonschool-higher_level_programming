c3833bc16852aa12b528769b0cccc2f16532b00b
C-Mauro <maurocamila499@gmail.com>
2023-02-17 11:28:37 -0800

    first commit

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''define a square'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''define a square'''
+    def __init__(self, size, x=0, y=0, id=None):
+        '''initialize variables'''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        '''returns string'''
+        id = self.id
+        x = self.x
+        y = self.y
+        size = self.height
+        return "[Square] ({}) {}/{} - {}".format(id, x, y, size)
