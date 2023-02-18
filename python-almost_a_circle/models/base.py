af13410560a1e806dd6ddac65e0a54cd25706d33
C-Mauro <maurocamila499@gmail.com>
2023-02-18 11:44:43 -0800

    fix the empty list return and add comment at the from_json_string() staticmethod

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -38,7 +38,8 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        '''json string to dictionary'''
         if json_string is None:
-            return "[]"
+            return []
         else:
             return json.loads(json_string)
