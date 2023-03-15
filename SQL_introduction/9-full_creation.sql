969c1d0344f7136359ede0335b06c4e6d9883fb2
C-Mauro <maurocamila744@gmail.com>
2023-03-15 16:19:38 -0700

    sintax

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -8,19 +8,19 @@ CREATE TABLE IF NOT EXISTS second_table (
 INSERT INTO
     `second_table` (`id`, `name`, `score`)
 VALUES
-(1, “ John ”, 10);
+    (1, "John", 10);
 
 INSERT INTO
     `second_table` (`id`, `name`, `score`)
 VALUES
-(2, "Alex", 3);
+    (2, "Alex", 3);
 
 INSERT INTO
     `second_table` (`id`, `name`, `score`)
 VALUES
-(3, "Bob", 14);
+    (3, "Bob", 14);
 
 INSERT INTO
     `second_table` (`id`, `name`, `score`)
 VALUES
-(4, "George", 8);
+    (4, "George", 8);
