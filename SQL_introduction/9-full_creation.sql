289274351de77b3adb2c73c320cf7497c0e14b8e
C-Mauro <maurocamila744@gmail.com>
2023-03-15 16:15:30 -0700

    sintax

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Create a table and add new records
-CREATE TABLE IF NOT EXISTS `second_table` (
+CREATE TABLE IF NOT EXISTS second_table (
     `id` INT,
     `name` VARCHAR(256),
     `score` INT
