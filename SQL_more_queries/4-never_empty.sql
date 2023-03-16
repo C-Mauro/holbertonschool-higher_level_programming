cfa1176624b3148306a70fe313e4c19c50095f3e
C-Mauro <maurocamila744@gmail.com>
2023-03-16 09:46:34 -0700

    quotes

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,6 @@
 -- create a table with id not null
 
-CREATE TABLE IF NOT EXISTS 'hbtn_test_db_4.id_not_null' (
+CREATE TABLE IF NOT EXISTS id_not_null (
 	'id' INT NOT NULL, 
 	'name' VARCHAR(256)
 );
