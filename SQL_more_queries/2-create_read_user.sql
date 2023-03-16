7ffb71f5814c6feef737411b0e72bf6fd7a75fae
C-Mauro <maurocamila744@gmail.com>
2023-03-16 07:22:24 -0700

    add @

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 -- create user user_0d_2
 
 CREATE database IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER IF NOT EXISTS `user_0d_2` IDENTIFIED BY `hbtn_0d_2`;
+CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY `hbtn_0d_2`;
 GRANT SELECT ON *.* `user_0d_1`@`localhost`;
