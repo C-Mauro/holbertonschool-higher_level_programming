ac948023ff75b62388d4c9279cacda1154b7f52a
C-Mauro <maurocamila744@gmail.com>
2023-03-16 07:08:14 -0700

    sintax

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- create a user 
 -- user_0d_1
-CREATE USER IF NOT EXIST 'user_0d_1`@`localhost' IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1`@`localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO  'user_0d_1'@'localhost';
