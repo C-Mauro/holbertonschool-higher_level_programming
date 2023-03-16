4841f231d882fcb39fdc61705fe991c4472abb4c
C-Mauro <maurocamila744@gmail.com>
2023-03-16 06:31:21 -0700

    add localhost

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- list all privileges of the mySQL users
 -- users: user_0d_1 and user_0d_2
-SHOW GRANTS FOR user_0d_1;
-SHOW GRANTS FOR user_0d_2;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
