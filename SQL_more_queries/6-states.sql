17bc16ab4434aec9f921e7897e2b91684000b195
C-Mauro <maurocamila744@gmail.com>
2023-03-16 10:14:28 -0700

    first commit

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- create a database and table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INT UNIQUE NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
+);
