c1808b99a0ca99a9735f4ee4b28f9f82965b8cea
C-Mauro <maurocamila744@gmail.com>
2023-03-16 07:26:47 -0700

    forgot database name

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@
 
 CREATE database IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY `hbtn_0d_2`;
-GRANT SELECT ON *.* `user_0d_2`@`localhost`;
+GRANT SELECT ON `hbtn_0d_2` TO `user_0d_2`@`localhost`;
