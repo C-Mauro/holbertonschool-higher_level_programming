09ec19524c271d76b18d09dcc025a05ee571536a
C-Mauro <maurocamila744@gmail.com>
2023-03-16 07:56:00 -0700

    quotes

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- create table force_name.
 
-CREATE TABLE IF NOT EXISTS hbtn_test_db_3.force_name (
+CREATE TABLE IF NOT EXISTS 'hbtn_test_db_3.force_name' (
 	'id' INT,
 	'name' VARCHAR(256) NOT NULL
 );
