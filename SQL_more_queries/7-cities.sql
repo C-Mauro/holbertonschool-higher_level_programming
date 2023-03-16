65bac16ac3b7504e4571d899950974f73f096064
C-Mauro <maurocamila744@gmail.com>
2023-03-16 11:09:05 -0700

    first commit

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- create a database and a table 
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL FOREING KEY REFERENCES state(id),
+	name VARCHAR(256) NOT NULL
+);
