de600e20bef03254f78d4983413a8a2613ec08f4
C-Mauro <maurocamila744@gmail.com>
2023-03-16 07:34:46 -0700

    uppercase database

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- create a database hbtn_0d_2
 -- create user user_0d_2
 
-CREATE database IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'hbtn_0d_2';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2`@`localhost';
