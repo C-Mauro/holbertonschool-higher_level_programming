c224d54311e31fcbf9734dfd887270354c567210
C-Mauro <maurocamila744@gmail.com>
2023-03-18 12:50:03 -0700

    rm use

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,6 @@
 -- create a database and a table 
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	state_id INT NOT NULL FOREING KEY (state_id) REFERENCES state(id),
