a2c05e988740f048c5bfd7b197b482030cc740e3
C-Mauro <maurocamila744@gmail.com>
2023-03-18 12:46:32 -0700

    add use statement

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 -- create a database and a table 
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	FOREING KEY (state_id) REFERENCES state(id)
+	state_id INT NOT NULL FOREING KEY (state_id) REFERENCES state(id),
+	name VARCHAR(256) NOT NULL
 );
