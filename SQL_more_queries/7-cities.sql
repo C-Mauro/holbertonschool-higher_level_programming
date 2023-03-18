0b73b03367efc9339dc77a2bd282b97e0452959a
Camila Mauro <105792882+C-Mauro@users.noreply.github.com>
2023-03-18 16:58:23 -0300

    Update 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
-    FOREIGN KEY KEY (state_id) REFERENCES hbtn_0d_usa.state(id),
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.state(id),
     name VARCHAR(256) NOT NULL
 );
