e92be75bc6a40ddadc82a28424f2d567e81ba05f
C-Mauro <maurocamila744@gmail.com>
2023-03-18 12:56:39 -0700

    foreign

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- create a database and a table 
-
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL FOREING KEY (state_id) REFERENCES hbtn_0d_usa.state(id),
-	name VARCHAR(256) NOT NULL
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL,
+    FOREIGN KEY KEY (state_id) REFERENCES hbtn_0d_usa.state(id),
+    name VARCHAR(256) NOT NULL
 );
