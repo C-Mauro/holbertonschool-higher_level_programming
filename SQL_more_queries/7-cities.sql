69a87fe61a861966d5069b3368b9a9bfb1ad293b
C-Mauro <maurocamila744@gmail.com>
2023-03-18 12:52:58 -0700

    hbtn_0d_usa

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- create a database and a table 
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL FOREING KEY (state_id) REFERENCES state(id),
+	state_id INT NOT NULL FOREING KEY (state_id) REFERENCES hbtn_0d_usa.state(id),
 	name VARCHAR(256) NOT NULL
 );
