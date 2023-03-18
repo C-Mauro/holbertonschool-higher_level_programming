b4b18e968d475c1e0a1d6592552a2e3e31867893
C-Mauro <maurocamila744@gmail.com>
2023-03-18 12:02:36 -0700

    add database

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- create a database and table
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT UNIQUE PRIMARY KEY AUTOINCREMENT,
 	name VARCHAR(256) NOT NULL,
 );
