fd8c132a440170f81f68953475feb6eefaefe65d
C-Mauro <maurocamila744@gmail.com>
2023-03-18 12:17:55 -0700

    auto_increment

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- create a database and a table 
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	state_id INT NOT NULL FOREING KEY REFERENCES state(id),
 	name VARCHAR(256) NOT NULL
