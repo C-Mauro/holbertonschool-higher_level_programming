c3d3de53c296ba075d7ae56b8eaccd2d6e219a12
C-Mauro <maurocamila744@gmail.com>
2023-03-18 12:06:29 -0700

    add quotes when state table is created

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- create a database and table
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
 	id INT UNIQUE PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
 );
