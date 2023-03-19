38d8e200a1ba95fbc9d3b4ad02d50a90e49f4eec
C-Mauro <maurocamila744@gmail.com>
2023-03-19 05:06:43 -0700

    sintax

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 -- create user user_0d_2
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-DROP USER IF EXIST 'user_0d_2'@'localhost';
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'hbtn_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES; 
