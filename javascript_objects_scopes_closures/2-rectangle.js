0aeae2cf8437cb462aaddd6240d6827bba9f5d47
C-Mauro <maurocamila744@gmail.com>
2023-04-18 08:00:27 -0700

    fix conditional

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor(w, h) {
-    this.width = w;
-    this.height = h;
-    if (w <= 0 || h <= 0) {
-      return new Rectangle();
+  constructor (w, h) {
+    if (w > 0 && h < 0 && typeof h === 'number' && typeof w === 'number') {
+      this.width = w;
+      this.height = h;
     }
   }
 };
-
