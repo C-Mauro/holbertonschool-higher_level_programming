d263deacb2cb7743a8bad958f46b2d1497c7717d
C-Mauro <maurocamila744@gmai.com>
2023-04-19 15:01:21 +0000

    rm '\n'

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 // 'fs' is the native module from Node.js
 
-fs.writeFile(process.argv[2], process.argv[3], '\n', 'utf8', function (error) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (error) {
     if (error) {
         console.log(error);
     }
