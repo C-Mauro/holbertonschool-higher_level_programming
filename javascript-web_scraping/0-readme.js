854c1904fd11ff792f89fdfd88efdbde3d4413bf
C-Mauro <maurocamila744@gmai.com>
2023-04-19 13:27:10 +0000

    first commit

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(data);
+});
