84acde427216c0fe1a48d00cebd43ded6c1cec58
C-Mauro <maurocamila744@gmai.com>
2023-04-22 11:47:00 +0000

    semistandard

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const request = require('request');
-const fs = require('fs')
+const fs = require('fs');
 
 request(process.argv[2], function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        fs.writeFileSync(process.argv[3], body, 'utf8');
-    }
+  if (!error && response.statusCode === 200) {
+    fs.writeFileSync(process.argv[3], body, 'utf8');
+  }
 });
